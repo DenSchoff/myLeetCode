9687747ce1b8258add60931dab4b4b62b96d1349
DenSchoff <146877200+DenSchoff@users.noreply.github.com>
2025-09-04 13:17:29 +0300

    Create 66. Plus One.cpp

---
+++ 66. Plus One.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size()-1] += 1;
+        for (int i = digits.size() - 1; i > 0; i--)
+        {
+            if (digits[i] >= 10)
+            {
+                digits[i] -= 10;
+                digits[i-1] += 1;
+            }
+        }
+        if (digits[0] >= 10)
+        {
+            digits[0] -= 10;
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
