0fca97c6fe1c4099b8497d47f63278b1d888501d
DenSchoff <146877200+DenSchoff@users.noreply.github.com>
2025-09-05 16:28:09 +0300

    Update 35. Search Insert Position.cpp

--- 35. Search Insert Position.cpp
+++ 35. Search Insert Position.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if (target < nums[0]) return 0;
        for (int i = 0; i < nums.size(); i++)
        {
         if (target <= nums[i]) return i;
