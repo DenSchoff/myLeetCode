1e7f4198df0f5127ab9e78b0975f27d80122207a
DenSchoff <146877200+DenSchoff@users.noreply.github.com>
2025-09-09 11:12:43 +0300

    Update 41. First missing positive.cpp

--- 41. First missing positive.cpp
+++ 41. First missing positive.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int answer = 1;
-        int i = nums.size()-1;
-        int j = 0;
-        while (i >= 0 && j < nums.size())
+        int i = 0;
+        std::sort(nums.begin(), nums.end());
+        while (i < nums.size())
         {
             if (nums[i] == answer) 
             {
                 answer++;
-                nums.erase(nums.begin() + i);
-                i = nums.size()-1;
-                j=0;
             }
-            else i--;
-            if (nums[j] == answer) 
-            {
-                answer++;
-                nums.erase(nums.begin() + j);
-                i = nums.size()-1;
-                j=0;
-            }
-            else j++;
+            i++;
         }
         return answer;
     }
